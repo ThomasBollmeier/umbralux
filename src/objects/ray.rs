2be8aa6617f1881b183fb66def1428f63efba2c2
Thomas Bollmeier <developer@thomas-bollmeier.de>
2021-08-13 22:31:17 +0200

    implemented ray - sphere intersection - with partner info

--- src/objects/ray.rs
+++ src/objects/ray.rs
@@ -1,4 +1,5 @@
 use crate::core::{Point, Vector};
+use std::any::Any;
 
 pub struct Ray {
     origin: Point,
@@ -21,4 +22,31 @@ impl Ray {
     pub fn position(&self, t: f64) -> Point {
         self.origin + t * self.direction
     }
+}
+
+pub trait Object3D {
+    fn as_any(&self) -> &dyn Any;
+    fn intersect<'a, 'b>(&'b self, ray: &'a Ray) -> Vec<Intersection<'a, 'b>>;
+}
+
+pub struct Intersection<'a, 'b> {
+    ray: &'a Ray,
+    t: f64,
+    partner: &'b dyn Object3D,
+}
+
+impl <'a, 'b> Intersection<'a, 'b> {
+
+    pub fn new(ray: &'a Ray, t:f64, partner: &'b dyn Object3D) -> Self {
+        Intersection { ray, t, partner }
+    }
+
+    pub fn position(&self) -> Point {
+        self.ray.position(self.t)
+    }
+
+    pub fn partner(&self) -> &dyn Object3D {
+        self.partner
+    }
+
 }
--- src/objects/sphere.rs
+++ src/objects/sphere.rs
@@ -1,6 +1,8 @@
 use crate::core::Point;
-use crate::objects::ray::Ray;
+use crate::objects::ray::{Ray, Intersection, Object3D};
+use std::any::Any;
 
+#[derive(PartialEq, Debug)]
 pub struct Sphere {
     origin: Point,
     radius: f64,
@@ -8,7 +10,7 @@ pub struct Sphere {
 
 impl Sphere {
     pub fn new(origin: Point, radius: f64) -> Sphere {
-        Sphere{ origin, radius}
+        Sphere { origin, radius }
     }
 
     pub fn intersects_with_ray_at(&self, ray: &Ray) -> Vec<f64> {
@@ -32,11 +34,25 @@ impl Sphere {
     }
 }
 
+impl Object3D for Sphere {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    
+    fn intersect<'a, 'b>(&'b self, ray: &'a Ray) -> Vec<Intersection<'a, 'b>> {
+        let ts = self.intersects_with_ray_at(&*ray);
+        ts.iter().map(|t| {
+            Intersection::new(ray, *t, self)
+        }).collect()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::objects::ray::Ray;
+    use crate::objects::ray::{Ray, Object3D};
     use crate::core::{Vector, Point};
     use crate::objects::sphere::Sphere;
+    use crate::testutil::assert_point_eq;
 
     #[test]
     fn ray_intersects_with_sphere_at_two_points() {
@@ -100,4 +116,24 @@ mod tests {
         assert_eq!(ts[0], -6.0);
         assert_eq!(ts[1], -4.0);
     }
+
+    #[test]
+    fn intersection_with_sphere_at_two_points() {
+
+        let r = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0));
+        let s = Sphere::new(Point::new(0.0, 0.0, 0.0), 1.0);
+
+        let intersections = s.intersect(&r);
+
+        assert_eq!(intersections.len(), 2);
+        assert_point_eq(intersections[0].position(), Point::new(0.0, 0.0, -1.0));
+        assert_point_eq(intersections[1].position(), Point::new(0.0, 0.0, 1.0));
+
+        let mut s2 = intersections[0].partner().as_any().downcast_ref::<Sphere>().unwrap();
+        assert_eq!(&s, s2);
+        s2 = intersections[1].partner().as_any().downcast_ref::<Sphere>().unwrap();
+        assert_eq!(&s, s2);
+
+    }
+
 }
