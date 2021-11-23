a70ef72d44bd280e54b9b37ae443fb3c75e56e37
Thomas Bollmeier <developer@thomas-bollmeier.de>
2021-11-23 22:21:44 +0100

    World implementation contd.

--- src/objects/world.rs
+++ src/objects/world.rs
@@ -4,20 +4,20 @@ use crate::objects::object3d::Object3D;
 
 pub struct World {
     objects: Vec<Rc<dyn Object3D>>,
-    lights: Vec<PointLight>,
+    lights: Vec<Rc<PointLight>>,
 }
 
 impl World {
-    
+
     pub fn new() -> World {
         World {
             objects: vec![],
-            lights: vec![]
+            lights: vec![],
         }
     }
 
-    pub fn add_light(&mut self, light: PointLight) {
-        self.lights.push(light);
+    pub fn add_light(&mut self, light: &Rc<PointLight>) {
+        self.lights.push(light.clone());
     }
 
     pub fn add_object(&mut self, object: &Rc<dyn Object3D>) {
@@ -28,5 +28,16 @@ impl World {
 
 #[cfg(test)]
 mod tests {
+    use crate::objects::world::World;
+
+    #[test]
+    fn creating_a_world() {
+
+        let world = World::new();
+
+        assert!(world.objects.is_empty());
+        assert!(world.lights.is_empty());
+
+    }
 
 }
